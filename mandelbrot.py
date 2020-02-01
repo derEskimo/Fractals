68bc699ee694f5cce468f7c4e4fcf35ddffd8ca7
derEskimo <hannes.zakes@gmx.de>
2020-02-01 15:42:41 +0100

    Fix wrong imaginary part

--- mandelbrot.py
+++ mandelbrot.py
@@ -11,19 +11,20 @@ def check_mb_set(x, y):
     real = imag = counter = 0
     if debug:
         print("**************************************************")
-        print("Check Point ({0:},{1:})".format(x, y))
+        print("Check Point ({0:}/{1:})".format(x, y))
     while counter < MAX_ITERATIONS:
         if debug: 
             print("--------------------------------------------------")
-            print("%8s"%"Complex Number: z={0:+f}{1:+f}i".format(real, imag))
+            print("Iteration-Step: {0:}".format(counter))
+            print("Complex Number: z={0:+f}{1:+f}i".format(real, imag))
         dist = math.sqrt(real**2 + imag**2)
         if debug: print("Distance to origin is: {0:}".format(dist))
-        real, imag = real**2 + imag**2 + x, real*imag + y
+        real, imag = real**2 + imag**2 + x, 2*real*imag + y
         counter += 1
         if dist > 2:
-            if debug: print("Distance not converging. Point ({0:},{1:}) is NOT in Mandelbrot Set".format(x, y))
+            if debug: print("Distance not converging. Point ({0:}/{1:}) is NOT in Mandelbrot Set".format(x, y))
             return False, counter
-    if debug: print("Distance not converging. Point ({0:},{1:}) is NOT in Mandelbrot Set".format(x, y))
+    if debug: print("Distance converging. Point ({0:}/{1:}) is in Mandelbrot Set".format(x, y))
     return True, 0   
 
 for x in X_RANGE:
